5612127dfdf37c6448755a6c76d52601651d0411
liaoyiqi <liaoyq@saichen.cn>
2025-02-13 16:28:08 +0800

    美团模版提交

--- src/App.js
+++ src/App.js
@@ -1,28 +1,4 @@
-import {useSelector, useDispatch} from "react-redux";
-import {addition, decrement} from "./store/modules/counterStore";
-import {useEffect} from "react";
-import {fetchChannels, setChannels} from "./store/modules/channelStore";
-
 function App() {
-    const {count} = useSelector((state) => state.counter)
-    const dispatch = useDispatch()
-    const {channelList} = useSelector(state => state.channel)
-    const params = '传值'
-    useEffect(() => {
-        dispatch(fetchChannels())
-    }, [dispatch]);
-    return (
-        <div>
-            <button onClick={() => dispatch(decrement())}>-</button>
-            {count}
-            <button onClick={() => dispatch(addition())}>+</button>
-            <button onClick={() => dispatch(setChannels([]))}>清空</button>
-            <button onClick={() => dispatch(fetchChannels())}>获取</button>
-            <ul>
-                {channelList.map(item => <li key={item.id}>{item.name}</li>)}
-            </ul>
-        </div>
-    )
 }
 
 export default App
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,7 @@
 import {configureStore} from "@reduxjs/toolkit";
-import counterReducer from "./modules/counterStore";
-import channelReducer from "./modules/channelStore";
 
 const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-        channel: channelReducer
-    }
+    reducer: {}
 })
 
 export default store

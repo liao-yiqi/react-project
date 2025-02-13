1bd4fb49ec21059bf31b30d54b750e4a7bcbebc1
liaoyiqi <liaoyq@saichen.cn>
2025-02-13 15:56:00 +0800

    redux提交

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import store from "./store";
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
+    // React.StrictMode->严格模式
+    // <React.StrictMode>
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    // </React.StrictMode>
 );
 

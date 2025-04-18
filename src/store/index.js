b2a5098b52819a0a98c05dacc5aa6b7576ca1059
liaoyiqi <liaoyq@saichen.cn>
2025-04-18 10:31:38 +0800

    项目初始化

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import userReducer from "@/store/modules/user.js";
-
-export default configureStore({
-    reducer: {
-        user: userReducer
-    }
-})
